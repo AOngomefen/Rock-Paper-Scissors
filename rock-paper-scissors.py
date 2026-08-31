058c6a73841627069ea4e73de7256402b10fd7c4
drea.m <148658944+AOngomefen@users.noreply.github.com>
2026-08-31 12:43:10 -0400

    making turn decisions clarifying game logic

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -1,9 +1,9 @@
 '''
 starting the format of the rock, paper, scissors game.
-
+ASCII drawing curtesy of (github: wynand1004)
 '''
 
-rock = ("""
+rock_gesture = ("""
     _______
 ---l   ____)
       (_____)
@@ -13,7 +13,7 @@ rock = ("""
 
 """)
 
-paper = ("""
+paper_gesture = ("""
     _______
 ---l    ____)____
            ______)
@@ -23,7 +23,7 @@ paper = ("""
 
 """")
 
-scissors = (""""
+scissors_gesture = (""""
    _______
 ---l   ____)____
           ______)
@@ -44,14 +44,24 @@ Would Like To Play Some Tic-Tac-Toe? (Y/N)
 ----------------------------------------------
 """)
 
-while (greeting == Y){
+Score = 0
+Turn = 0
+# choose if turn based or player based ?
+while (Turn != 3){
         gesture = input("Play your hand! (rock, paper, scissors) ")
         #random -> select gesture
         
         switch (gesture){
             case rock:
-                if (computer_play = rock){
-                print("----------------------------------------------/nscore is tied!")
-         }
+                if (computer_play == rock){
+                print("----------------------------------------------\nTry again!")
+                # play turn again
+                }
+                else (computer_play == scissors) {
+                print("----------------------------------------------\nYou win!")
+                Score++
+                print("Score is ", Score, "Hmm... Best two out of three?")
+                
+                 }
          }
          }
