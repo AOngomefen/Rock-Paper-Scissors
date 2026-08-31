9d97cfc181d178db3ff597d495d056f84a041ebb
drea.m <148658944+AOngomefen@users.noreply.github.com>
2026-08-30 23:42:53 -0400

    adding ascii drawing for the hand gestures and plays

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -3,3 +3,37 @@ starting the format of the rock, paper, scissors game.
 
 '''
 
+rock = "
+    _______
+---l   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+
+"
+
+paper = "
+    _______
+---l    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+
+"
+
+scissors = "
+   _______
+---l   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+
+"
+'''
+using ascii drawing for the hand gestures and plays
+
+'''
+
